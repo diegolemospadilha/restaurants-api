4ba578a377e9b14abeeb1a0ebf61e99a6cf91c08
Diego Lemos Padilha <dlemos.padilha@gmail.com>
2022-12-27 19:09:12 -0300

    fix is open endpoint

--- src/domain/use-cases/IsOpenRestaurantUseCase.ts
+++ src/domain/use-cases/IsOpenRestaurantUseCase.ts
@@ -7,15 +7,38 @@ export class IsOpenRestaurantUseCase {
         readonly repository: RestaurantOut
     ){}
 
+    private getTimeInSeconds(hour: string){
+        const splitHours =  hour.split(':'); 
+        return parseInt(splitHours[0]) * 3600 + parseInt(splitHours[1]) * 60 + parseInt(splitHours[2])
+    }
+
+    private checkIfRestaurantIsOpenByOpeningHour(timeToBeCompared: string, day: any){
+        const dayOpenAtInSeconds = this.getTimeInSeconds(day.opens_at)
+        const dayClosesAtInSeconds = this.getTimeInSeconds(day.closes_at)
+
+        const timeInSeconds = this.getTimeInSeconds(timeToBeCompared)
+
+        const isOpen = timeInSeconds > dayOpenAtInSeconds && timeInSeconds <= dayClosesAtInSeconds;
+        return isOpen;
+    }
+
     async execute(id: string, datetime: Date): Promise<RestaurantIsOpenOutput> {
         const data = await this.repository.getById(id);
 
         if(!data){
             throw new RestaurantNotFound(id);
         }
-       
-        const { id:restaurantId, name, description } = data;
-        const output = { isOpen: true };
+
+        const dayToBeSearched = new Date(datetime);
+        const dayOfWeek = dayToBeSearched.getDay();
+        const time = dayToBeSearched.toLocaleTimeString()
+        const dayExists = data.filter((restaurantDay: any) => restaurantDay.day === dayOfWeek)
+
+        if(!dayExists) return { isOpen: false}
+
+        const isOpen = dayExists.some((day: any) => this.checkIfRestaurantIsOpenByOpeningHour(time, day));
+
+        const output = { isOpen };
         
         return output;
     }
